62bad6cef361d18327627ea09ca574aa5b4af4fb
Kenta Kato <kenta.kato@theport.jp>
2016-12-07 21:12:15 +0900

    add ava

---
+++ frontend/config/test/setup-browser-env.js
@@ -0,0 +1,4 @@
+// https://github.com/avajs/ava/blob/master/docs/recipes/browser-testing.md
+
+import browserEnv from 'browser-env';
+browserEnv(['window', 'document', 'navigator']);
--- frontend/test/Modal.test.js
+++ frontend/test/Modal.test.js
@@ -1,27 +1,15 @@
-import assert from 'assert';
+import {describe} from 'ava-spec';
 import sinon from 'sinon';
 import React from 'react';
 import { mount } from 'enzyme';
-import Modal from '../javascripts/Modal.js';
+import Modal from '../javascripts/Modal';
 
-describe('<Modal />', () => {
-  it('閉じるボタンを押してonRequestCloseのコールバックが呼ばれるか', () => {
+describe('<Modal />', it => {
+  it('閉じるボタンを押してonRequestCloseのコールバックが呼ばれるか', t => {
     const props = { onRequestClose: sinon.spy() };
     const wrapper = mount(<Modal {...props} />);
 
     wrapper.find('.modal__close').simulate('click');
-    assert(props.onRequestClose.called === true);
-  });
-
-  it('マウントされてから三秒後にonRequestCloseのコールバックが呼ばれるか', () => {
-    const clock = sinon.useFakeTimers();
-    const props = { onRequestClose: sinon.spy() };
-    const wrapper = mount(<Modal {...props} />);
-
-    clock.tick(1000);
-    assert(props.onRequestClose.called === false);
-
-    clock.tick(2000);
-    assert(props.onRequestClose.called === true);
+    t.is(props.onRequestClose.called, true);
   });
 });
